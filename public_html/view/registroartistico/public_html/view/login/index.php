14ff6a9c28b0789e9b4b144a9d784ac860765180
ERIC PEREIRA <eric.pereira.swp@outlook.com>
2025-12-08 20:18:45 -0300

    Update index.php

--- public_html/view/registroartistico/public_html/view/login/index.php
+++ public_html/view/registroartistico/public_html/view/login/index.php
@@ -35,7 +35,7 @@
                                     <button onclick="clientLoginSimpleSend()" type="submit" class="btn btn-secondary">Entrar</button>
                                     <button onclick="clientKeySimpleSend()" type="submit" class="btn btn-secondary">Receber Chave</button>
                                 </div>
-                                <a href="<?php echo $config["source"] . 'cadastro/usuario/' ?>" class="btn btn-link">Esqueceu a Senha ou Novo Usuário!</a>
+                                <a href="<?php echo $config["source"] . 'view/cadastro/usuario/' ?>" class="btn btn-link">Esqueceu a Senha ou Novo Usuário!</a>
                             </div>
                         </div>
                     </div>
